c6b44d4aa6907f4547fa849ddadbff2677c2b7b0
FaridRjb <faridrajabi7@gmail.com>
2021-08-01 16:09:00 +0430

    Update AboutActivity

--- app/src/main/java/com/faridrjb/whattocook/activities/AboutActivity.java
+++ app/src/main/java/com/faridrjb/whattocook/activities/AboutActivity.java
@@ -23,13 +23,24 @@ public class AboutActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        Intent intent;
         switch (view.getId()) {
             case R.id.icon_back:
                 finish();
                 break;
             case R.id.emailBtn:
-                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent = new Intent(Intent.ACTION_SENDTO);
                 intent.setData(Uri.parse("mailto:" + "faridrajabi7@gmail.com"));
+                startActivity(intent);
+                break;
+            case R.id.linkedinBtn:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.linkedin.com/in/faridrjb/"));
+                startActivity(intent);
+                break;
+            case R.id.twitterBtn:
+                intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://twitter.com/FaridRjb/"));
                 startActivity(intent);
                 break;
         }
