45fea26d438255a8e97a7afae04e4d45b397f3a2
FaridRjb <faridrajabi7@gmail.com>
2021-08-19 16:49:58 +0430

    Added PossibleFragment

--- app/src/main/java/com/faridrjb/whattocook/activities/MainActivity.kt
+++ app/src/main/java/com/faridrjb/whattocook/activities/MainActivity.kt
@@ -1,40 +1,21 @@
 package com.faridrjb.whattocook.activities
 
-import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import com.faridrjb.whattocook.data.DatabaseHelper
-import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import android.annotation.SuppressLint
 import android.os.Bundle
 import com.faridrjb.whattocook.R
-import android.content.SharedPreferences
 import android.content.Intent
 import android.os.Handler
 import android.view.View
 import android.view.View.GONE
-import com.faridrjb.whattocook.activities.IntroSliderActivity
-import com.faridrjb.whattocook.activities.AboutActivity
 import com.faridrjb.whattocook.fragments.FavoriteFragment
-import android.view.View.OnFocusChangeListener
-import com.faridrjb.whattocook.activities.SearchActivity
-import com.faridrjb.whattocook.activities.StorageActivity
-import com.faridrjb.whattocook.activities.PosFavActivity
-import com.faridrjb.whattocook.Food
-import com.faridrjb.whattocook.FoodsChecker
-import androidx.recyclerview.widget.RecyclerView
-import com.faridrjb.whattocook.recyclerviewadapters.PossibleRVAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
-import android.view.ViewAnimationUtils
-import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.widget.Toolbar
-import com.faridrjb.whattocook.databinding.ActivityIntroSliderBinding
+import com.faridrjb.whattocook.fragments.PossibleFragment
 import com.faridrjb.whattocook.databinding.ActivityMainBinding
 import com.faridrjb.whattocook.databinding.DialogFragHelpBinding
 import java.io.IOException
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -84,7 +65,9 @@ class MainActivity : AppCompatActivity() {
         }
         //--------
 
-        // Favorite Fragment
+        // Fragments
+        supportFragmentManager.beginTransaction().replace(R.id.posFragContainer, PossibleFragment())
+            .commit()
         supportFragmentManager.beginTransaction().replace(R.id.favFragContainer, FavoriteFragment())
             .commit()
         //------------------
@@ -97,81 +80,10 @@ class MainActivity : AppCompatActivity() {
         binding.floatingButton.setOnClickListener(View.OnClickListener {
             startActivity(Intent(this@MainActivity, StorageActivity::class.java))
         })
-        binding.posMore.setOnClickListener {
-            val intent = Intent(this@MainActivity, PosFavActivity::class.java)
-            intent.putExtra("IntentToPosFav", "PossibleFoods")
-            startActivity(intent)
-        }
 
         // help btn
         binding.toolbar.helpBtn.setOnClickListener { showHelpDialog() }
         //----------
-    }
-
-    override fun onResume() {
-        loadPossibleList()
-        super.onResume()
-    }
-
-    private fun loadPossibleList() {
-        binding.posNotFoundTV.visibility = GONE
-        val allInits = ArrayList<String>()
-        val a = resources.getStringArray(R.array.hoboobaat_names)
-        val b = resources.getStringArray(R.array.ghallaat_names)
-        val c = resources.getStringArray(R.array.labaniaat_names)
-        val d = resources.getStringArray(R.array.miveh_sabzi_names)
-        val e = resources.getStringArray(R.array.chaashni_names)
-        val f = resources.getStringArray(R.array.kareh_roghan_names)
-        val g = resources.getStringArray(R.array.protein_names)
-        val h = resources.getStringArray(R.array.others_names)
-        val j = resources.getStringArray(R.array.khoshkbar_names)
-        val total = arrayOf(a, b, c, d, e, f, g, h, j)
-        for (category in total) {
-            for (item in category) {
-                allInits.add(item)
-            }
-        }
-        val preferences = getSharedPreferences("Storage", MODE_PRIVATE)
-        val initsInStorage = ArrayList<String>()
-        for (item in allInits) {
-            if (preferences.getBoolean(item, false)) {
-                initsInStorage.add(item)
-            }
-        }
-        var possibleFoods = ArrayList<Food>()
-        val firstFivePossibleFoods = ArrayList<Food>()
-        possibleFoods = FoodsChecker().possibleFoods(this, initsInStorage, db!!.getFood(""))
-        for (i in possibleFoods.indices) { // just show first 5 items
-            firstFivePossibleFoods.add(possibleFoods[i])
-            if (firstFivePossibleFoods.size == 5) break
-        }
-        if (firstFivePossibleFoods.size == 0) binding.posNotFoundTV.visibility = View.VISIBLE
-        binding.possibleRV.adapter = PossibleRVAdapter(this, firstFivePossibleFoods)
-        binding.possibleRV.layoutManager = LinearLayoutManager(this)
-    }
-
-    private fun startCircularReveal() {
-        val fB = binding.floatingButton
-        val rL = binding.revealLayout
-        val centerX = (fB.right + fB.left) / 2
-        val centerY = (fB.bottom + fB.top) / 2
-        val endRadius = Math.hypot(rL.width.toDouble(), rL.height.toDouble())
-            .toFloat()
-        rL.visibility = View.VISIBLE
-        val revealAnimator = ViewAnimationUtils.createCircularReveal(
-            rL,
-            centerX, centerY, 0f, endRadius
-        )
-        revealAnimator.setDuration(200).start()
-        revealAnimator.addListener(object : Animator.AnimatorListener {
-            override fun onAnimationStart(animation: Animator) {}
-            override fun onAnimationEnd(animation: Animator) {
-                rL.visibility = View.INVISIBLE
-            }
-
-            override fun onAnimationCancel(animation: Animator) {}
-            override fun onAnimationRepeat(animation: Animator) {}
-        })
     }
 
     private fun showHelpDialog() {
---
+++ app/src/main/java/com/faridrjb/whattocook/fragments/PossibleFragment.kt
@@ -0,0 +1,98 @@
+package com.faridrjb.whattocook.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.faridrjb.whattocook.Food
+import com.faridrjb.whattocook.FoodsChecker
+import com.faridrjb.whattocook.R
+import com.faridrjb.whattocook.activities.PosFavActivity
+import com.faridrjb.whattocook.data.DatabaseHelper
+import com.faridrjb.whattocook.databinding.FragmentPossibleBinding
+import com.faridrjb.whattocook.recyclerviewadapters.PossibleRVAdapter
+import java.util.ArrayList
+
+class PossibleFragment : Fragment() {
+
+    private var _binding: FragmentPossibleBinding? = null
+    private val binding get() = _binding!!
+
+    var db: DatabaseHelper? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        db = DatabaseHelper(requireContext())
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPossibleBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.posMore.setOnClickListener {
+            val intent = Intent(activity, PosFavActivity::class.java)
+            intent.putExtra("IntentToPosFav", "PossibleFoods")
+            startActivity(intent)
+        }
+        loadPossibleList()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadPossibleList()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun loadPossibleList() {
+        binding.posNotFoundTV.visibility = View.GONE
+        val allInits = ArrayList<String>()
+        val a = resources.getStringArray(R.array.hoboobaat_names)
+        val b = resources.getStringArray(R.array.ghallaat_names)
+        val c = resources.getStringArray(R.array.labaniaat_names)
+        val d = resources.getStringArray(R.array.miveh_sabzi_names)
+        val e = resources.getStringArray(R.array.chaashni_names)
+        val f = resources.getStringArray(R.array.kareh_roghan_names)
+        val g = resources.getStringArray(R.array.protein_names)
+        val h = resources.getStringArray(R.array.others_names)
+        val j = resources.getStringArray(R.array.khoshkbar_names)
+        val total = arrayOf(a, b, c, d, e, f, g, h, j)
+        for (category in total) {
+            for (item in category) {
+                allInits.add(item)
+            }
+        }
+        val preferences = activity?.getSharedPreferences("Storage", AppCompatActivity.MODE_PRIVATE)
+        val initsInStorage = ArrayList<String>()
+        for (item in allInits) {
+            if (preferences!!.getBoolean(item, false)) {
+                initsInStorage.add(item)
+            }
+        }
+        var possibleFoods = ArrayList<Food>()
+        val firstFivePossibleFoods = ArrayList<Food>()
+        possibleFoods = FoodsChecker().possibleFoods(requireContext(), initsInStorage, db!!.getFood(""))
+        for (i in possibleFoods.indices) { // just show first 5 items
+            firstFivePossibleFoods.add(possibleFoods[i])
+            if (firstFivePossibleFoods.size == 5) break
+        }
+        if (firstFivePossibleFoods.size == 0) binding.posNotFoundTV.visibility = View.VISIBLE
+        binding.possibleRV.adapter = PossibleRVAdapter(requireContext(), firstFivePossibleFoods)
+        binding.possibleRV.layoutManager = LinearLayoutManager(requireContext())
+    }
+}
